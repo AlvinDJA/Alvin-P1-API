8e159f9287f68c41622345e11e8f242d07b20798
Alvin <alvindejesus88@gmail.com>
2020-10-05 18:45:24 -0400

    Cambios en Entidades, UI y Main

--- Entidades/ciudades.cs
+++ Entidades/ciudades.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Alvin_P1_API.Entidades
 {
-    public class rCiudades
+    public class Ciudades
     {
         [Key]
         public int ciudadId { get; set; }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Alvin_P1_API.Entidades;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using Alvin_P1_API.UI.Registros;
 namespace Alvin_P1_API
 {
     /// <summary>
@@ -25,9 +13,10 @@ namespace Alvin_P1_API
             InitializeComponent();
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void rCiudades_Click(object sender, RoutedEventArgs e)
         {
-
+            new rCiudades().Show();
         }
+
     }
 }
