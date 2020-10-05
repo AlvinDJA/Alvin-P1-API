97a4ea8ac733f1537254050f303b8a4bea9f37b7
Alvin <alvindejesus88@gmail.com>
2020-10-05 19:22:54 -0400

    Creacion Base de datos y programando botones

---
+++ Migrations/20201005230043_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Alvin_P1_API.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ciudades",
+                columns: table => new
+                {
+                    ciudadId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudades", x => x.ciudadId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ciudades");
+        }
+    }
+}
--- UI/Registros/rCiudades.xaml.cs
+++ UI/Registros/rCiudades.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Alvin_P1_API.BLL;
+using Alvin_P1_API.Entidades;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Alvin_P1_API.UI.Registros
 {
@@ -17,10 +21,87 @@ namespace Alvin_P1_API.UI.Registros
     /// </summary>
     public partial class rCiudades : Window
     {
+        private Ciudades ciudad;
         public rCiudades()
         {
             InitializeComponent();
+            ciudad = new Ciudades();
+            this.DataContext = ciudad;
+        }
+        private void Limpiar()
+        {
+            this.ciudad = new Ciudades();
+            this.DataContext = ciudad;
+        }
+        public void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+        private bool Validar()
+        {
+            bool esValido = true;
+            if (nombreTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, debe insertar un nombre", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            if (nombreTextBox.Text.Any(char.IsDigit))
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, no hay ciudades con numeros", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            return esValido;
+        }
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ciudad = CiudadesBLL.Buscar(Convert.ToInt32(ciudadIdTextBox.Text));
+            if (ciudad != null)
+                this.ciudad = ciudad;
+            else
+            {
+                this.ciudad = new Ciudades();
+                MessageBox.Show("No se ha encontrado", "Error",
+                   MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            this.DataContext = this.ciudad;
+        }
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (!Validar())
+                return;
+            var paso = CiudadesBLL.Guardar(this.ciudad);
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Transaccion exitosa", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CiudadesBLL.Eliminar(Convert.ToInt32(ciudadIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+
         }
 
     }
+
 }
+//Buscar
+//Nuevo
+//Guardar
+//Eliminar
+
