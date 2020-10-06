750e956bb2fa310c94bf13cac3fcc1dd341a2ad9
Alvin <alvindejesus88@gmail.com>
2020-10-05 20:03:14 -0400

    Tiempo vencido

---
+++ UI/Consultas/cCiudades.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Alvin_P1_API.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cCiudades.xaml
+    /// </summary>
+    public partial class cCiudades : Window
+    {
+        public cCiudades()
+        {
+            InitializeComponent();
+        }
+    }
+}
